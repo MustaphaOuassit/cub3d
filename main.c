9e59e58e1baa468c6d0fa2d7cc8dc15474286208
Mustapha Ouassit <mouassit@e1r3p2.1337.ma>
2021-02-07 19:22:20 +0100

    norm main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mouassit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 12:59:14 by mouassit          #+#    #+#             */
-/*   Updated: 2020/12/08 13:55:26 by mouassit         ###   ########.fr       */
+/*   Updated: 2021/02/07 19:21:50 by mouassit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,21 @@ int	main(int argc, char *argv[])
 	int		error;
 
 	error = 0;
-	if((argc == 2) || (argc == 3))
+	if ((argc == 2) || (argc == 3))
 	{
 		fd = open(argv[1], O_RDONLY);
 		if ((fd != -1) && (ft_extention(argv[1]) == 0))
 		{
-			error = ft_check_save(argc,argv,error);
-			if(error == 0)
-				ft_take_informations(fd,error);
+			error = ft_check_save(argc, argv, error);
+			if (error == 0)
+				ft_take_informations(fd, error);
 		}
 		else
 			ft_write_argv(argv[1]);
 	}
 	else
 	{
-		write(1,"Error\n",7);
-		write(1,"Wrong numbers of argc\n",22);
+		write(1, "Error\n", 7);
+		write(1, "Wrong numbers of argc\n", 22);
 	}
 }
